9cc644cc17417d9561c4144eb80ddad0c3807d9e
xinpengzhang <1125503414@qq.com>
2023-04-23 21:17:59 +0800

    路由和页面

---
+++ vue-admin-template-master/src/views/employees/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="employees-container">
+    <div class="app-container">
+      <h2>员工</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "employees",
+};
+</script>
+
+<style>
+</style>
---
+++ vue-admin-template-master/src/views/setting/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="setting-container">
+    <div class="app-container">
+      <h2>设置</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "setting",
+};
+</script>
+
+<style>
+</style>
